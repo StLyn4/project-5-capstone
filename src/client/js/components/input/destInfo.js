3cfce11547ec006a562a29ce31fdd50fa7a0a0d8
Vsevolod Volkov <st.lyn4@gmail.com>
2021-01-19 18:40:37 +0200

    Forms -> req star

--- src/client/js/components/input/destInfo.js
+++ src/client/js/components/input/destInfo.js
@@ -3,10 +3,10 @@ import {createJSXElement} from '../../jsx-runtime';
 export default (<div className="content">
   <p className="header">Travel information</p>
   <hr />
-  <label>Flight from: <input data-out-name="flightFrom"></input></label>
-  <label>Flight to: <input data-out-name="flightTo"></input></label>
+  <label className="req">Flight from: <input data-out-name="flightFrom"></input></label>
+  <label className="req">Flight to: <input data-out-name="flightTo"></input></label>
   <hr />
-  <label>Lodging address<br />(1 line - 1 address): <textarea data-out-name="lodg"></textarea></label>
+  <label className="req">Lodging address<br />(1 line - 1 address): <textarea data-out-name="lodg"></textarea></label>
   <label>Todo list<br />(1 line - 1 point): <textarea data-out-name="todo" data-out-can-be-empty></textarea></label>
   <hr />
   <label>Notes: <textarea data-out-name="notes" data-out-can-be-empty></textarea></label>
--- src/client/js/components/input/newDest.js
+++ src/client/js/components/input/newDest.js
@@ -3,18 +3,18 @@ import {createJSXElement} from '../../jsx-runtime';
 export default (<div className="content">
   <p className="header">Travel information</p>
   <hr />
-  <label>Destination: <input data-out-name="dest"></input></label>
+  <label className="req">Destination: <input data-out-name="dest"></input></label>
   <hr />
-  <label>Departing date: <input type="date" data-out-name="depDate"></input></label>
-  <label>Departing time: <input type="time" value="09:00" data-out-name="depTime"></input></label>
+  <label className="req">Departing date: <input type="date" data-out-name="depDate"></input></label>
+  <label className="req">Departing time: <input type="time" value="09:00" data-out-name="depTime"></input></label>
   <hr />
-  <label>End of stay date: <input type="date" data-out-name="endDate"></input></label>
-  <label>End of stay time: <input type="time" value="09:00" data-out-name="endTime"></input></label>
+  <label className="req">End of stay date: <input type="date" data-out-name="endDate"></input></label>
+  <label className="req">End of stay time: <input type="time" value="09:00" data-out-name="endTime"></input></label>
   <hr />
-  <label>Flight from: <input data-out-name="flightFrom"></input></label>
-  <label>Flight to: <input data-out-name="flightTo"></input></label>
+  <label className="req">Flight from: <input data-out-name="flightFrom"></input></label>
+  <label className="req">Flight to: <input data-out-name="flightTo"></input></label>
   <hr />
-  <label>Lodging address<br />(1 line - 1 address): <textarea data-out-name="lodg"></textarea></label>
+  <label className="req">Lodging address<br />(1 line - 1 address): <textarea data-out-name="lodg"></textarea></label>
   <label>Todo list<br />(1 line - 1 point): <textarea data-out-name="todo" data-out-can-be-empty></textarea></label>
   <hr />
   <label>Notes: <textarea data-out-name="notes" data-out-can-be-empty></textarea></label>
