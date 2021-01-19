35c3fa2a0adeb1c8be11bdb47d82805d5d50b3e2
Vsevolod Volkov <st.lyn4@gmail.com>
2021-01-20 01:38:14 +0200

    SW changes

--- src/client/index.js
+++ src/client/index.js
@@ -9,3 +9,15 @@ import 'regenerator-runtime/runtime';
 export * from './js/app.js';
 // Styles
 import './styles/style.scss';
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.info('Service worker registered.');
+     }).catch(registrationError => {
+       console.warn('Service worker registration failed:', registrationError.message);
+     });
+   });
+ }
